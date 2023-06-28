99730e28c705eaa0be99f4e8fb15b5cc13526624
Jesus David Salcedo <34328612+JDsystems@users.noreply.github.com>
2023-06-27 19:50:18 -0500

    Initial commit

---
+++ src/main/java/com/projectone/persistence/crud/CompraCrudRepository.java
@@ -0,0 +1,11 @@
+package com.projectone.persistence.crud;
+
+import com.projectone.persistence.entity.Compra;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CompraCrudRepository extends CrudRepository<Compra,Integer> {
+    Optional<List<Compra>> findByIdCliente(String idCliente);
+}
---
+++ src/main/java/com/projectone/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,29 @@
+package com.projectone.persistence.mapper;
+
+import com.projectone.domain.PurchaseItem;
+import com.projectone.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = { ProductMapper.class })
+public interface PurchaseItemMapper {
+
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+           // @Mapping(source = "total", target = "total"), se omite porque el source es igual al target
+            @Mapping(source = "estado", target = "active")
+
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true)
+    })
+    ComprasProducto toComprasProducto(PurchaseItem item);
+}
